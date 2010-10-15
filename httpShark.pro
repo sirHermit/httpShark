a112ff797f05d22767680ae4adc14d38ef4a1850
sirHermit <sir.hermit@gmail.com>
2010-10-15 16:43:43 +0300

    It is not a version of program. For now it is just simple web-browser.

---
+++ httpShark.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2010-08-28T15:56:52
+#
+#-------------------------------------------------
+
+QT       += core gui webkit network xmlpatterns
+
+TARGET = httpShark
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+RESOURCES +=
---
+++ mainwindow.cpp
@@ -0,0 +1,35 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QMessageBox"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QWebSettings *ws = QWebSettings::globalSettings();
+    ws->setAttribute(QWebSettings::JavascriptEnabled, true);
+    ws->setAttribute(QWebSettings::PluginsEnabled, true);
+    ws->setAttribute(QWebSettings::DeveloperExtrasEnabled, true);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_lineEdit_returnPressed()
+{
+    QString url_str = this->ui->lineEdit->text();
+    if (!url_str.startsWith("http://")){
+        url_str = "http://"+url_str;
+    }
+    QUrl url(url_str);
+    this->ui->webView->load(url);
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QMessageBox::about(this,"httpShark","Program for testing HTTP-responses.\n(c) Volodymyr Fedyk");
+}
