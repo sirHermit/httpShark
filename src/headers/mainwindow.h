3c898fec9bca8baf17f57ff35fb928c43e065569
sirHermit <sir.hermit@gmail.com>
2010-10-16 16:22:15 +0300

    Old version of httpShark uploaded. Changes in file structure coming.

--- src/headers/mainwindow.h
+++ src/headers/mainwindow.h
@@ -1,9 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <QtGui/QMainWindow>
+#include <QNetworkAccessManager>
+#include <QBuffer>
+#include <QSettings>
+#include <QTableWidget>
 
-namespace Ui {
+namespace Ui
+{
     class MainWindow;
 }
 
@@ -12,15 +17,30 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+    void Go();
+    void SaveRequestHeaders();
+    void setCellText(QTableWidget *someTable, int row, int column, QString cellText);
 
-private slots:
-    void on_actionAbout_triggered();
-    void on_lineEdit_returnPressed();
+    Ui::MainWindow *ui;
+    QNetworkAccessManager* m_netAccessManager;
+    bool m_isBusy;
+    QBuffer* m_buffer;
+    QSettings appSettings;
+
+ private slots:
+        void on_pushButton_clicked();
+        void on_tblHeaders_cellEntered(int row, int column);
+        void on_btnDelHeader_clicked();
+        void on_btnAddHeader_clicked();
+        void on_actionAbout_triggered();
+        void on_radioPOST_toggled(bool checked);
+        void on_radioGET_toggled(bool checked);
+        void on_btnGo_clicked();
+        void replyFinished(QNetworkReply*);
 };
 
 #endif // MAINWINDOW_H
